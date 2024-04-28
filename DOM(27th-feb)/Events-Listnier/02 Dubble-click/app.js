c89631e94788cf9a3508c0351b65331740c92d53
Adarsh754 <adarshbhagat459@gmail.com>
2024-04-28 12:57:26 +0530

    DOM project

---
+++ DOM(27th-feb)/Events-Listnier/02 Dubble-click/app.js
@@ -0,0 +1,8 @@
+// -----Dubble-Click Event--------------------
+
+const boxElement = document.getElementById("box");
+
+// adding a dblclick event listener to the element
+boxElement.addEventListener("dblclick", function () {
+  alert("doubleClick event is working properly 👍");
+});
---
+++ DOM(27th-feb)/Events-Listnier/05 On-keyPress/app.js
@@ -0,0 +1,10 @@
+//--------------- key Press events---------
+
+let inputBox = document.getElementById('input-box') 
+let display = document.getElementById('display')
+
+
+// // adding a keypress event listener to the inputbox
+inputBox.addEventListener('keypress', function(e){
+    display.innerText ="you have Pressed " + e.key
+})
